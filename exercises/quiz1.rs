6357692875e4c40073ecb7d568d99e6587a48ba2
CY <2845720572@qq.com>
2024-04-18 14:52:01 +0800

    update

--- exercises/quiz1.rs
+++ exercises/quiz1.rs
@@ -13,10 +13,18 @@
 //
 // No hints this time ;)
 
-// I AM NOT DONE
+
 
 // Put your function here!
-// fn calculate_price_of_apples {
+fn calculate_price_of_apples(i: i32) -> i32{
+    if i <=40{
+        i*2
+    }else{
+        i
+    }
+    
+}
+
 
 // Don't modify this function!
 #[test]
